c93caab02fc09ac2d06ca01c89819a595e8a1705
George Madrid <gmadrid@gmail.com>
2021-06-12 10:01:12 -0400

    Starting to work on a format.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 mod cal;
+mod rect;
 mod weeks;
 
 pub use cal::print_cal_for_month;
+pub use rect::PRect;
 pub use weeks::{weeks_for_month, LocalDate, WeekDesc};
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 use argh::FromArgs;
 use chrono::Local;
+use printpdf::*;
+use std::fs::File;
+use std::io::BufWriter;
+use weekly::PRect;
 
 #[derive(FromArgs)]
 /// Spew a calendar.
@@ -9,8 +13,56 @@ struct Args {
     prune: bool,
 }
 
+fn inches_to_mm(inches: f64) -> Mm {
+    Mm(inches * 25.4)
+}
+
+fn foo() {
+    let page_size = PRect::new(0.0, 0.0, inches_to_mm(11.0).0, inches_to_mm(8.5).0);
+    let (doc, page, layer) =
+        PdfDocument::new("test", Mm(page_size.width), Mm(page_size.height), "Layer 1");
+    let current_layer = doc.get_page(page).get_layer(layer);
+
+    let printable_area = page_size.inset(6.0, 5.0);
+
+    current_layer.set_outline_color(Color::Rgb(Rgb::new(0.0, 0.0, 0.0, None)));
+    current_layer.set_outline_thickness(0.0);
+
+    let col_rect = printable_area.with_width(printable_area.width / 8.0);
+
+    for mult in 0..8 {
+        current_layer.add_shape(col_rect.rmove(mult as f64 * col_rect.width, 0.0).shape());
+    }
+
+    //    current_layer.add_shape(col_rect.shape());
+
+    /*    let points = vec![
+        (Point::new(Mm(0.0), Mm(0.0)), false),
+         (Point::new(Mm(100.0), Mm(0.0)), false),
+          (Point::new(Mm(100.0), Mm(200.0)), false),
+        (Point::new(Mm(0.0), Mm(200.0)), false),
+    ];
+    let line = Line {
+    points: points,
+    is_closed: true,
+    has_fill: false,
+    has_stroke: true,
+    is_clipping_path: false,
+    };
+     */
+
+    //    current_layer.add_shape(line);
+
+    doc.save(&mut BufWriter::new(
+        File::create("/mnt/c/Users/gmadr/Desktop/foo.pdf").unwrap(),
+    ))
+    .unwrap();
+}
+
 fn main() {
     let args: Args = argh::from_env();
-    
+
     weekly::print_cal_for_month(Local::now().date(), args.prune);
+
+    foo();
 }

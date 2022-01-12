948675be397a674c95a539f20ee9a175a774af2d
George Madrid <gmadrid@gmail.com>
2022-01-11 22:19:52 -0500

    active uses TGrid and we delete the old code. Also, work around a small rounding error.

--- src/bin/active.rs
+++ src/bin/active.rs
@@ -1,7 +1,7 @@
 use printpdf::*;
 use weekly::{
-    save_one_page_document, AsPdfLine, Attributes, Colors, GridDescription, Instructions,
-    NumericUnit, TGrid, Unit, WLine, WRect,
+    save_one_page_document, AsPdfLine, Attributes, GridDescription, Instructions, NumericUnit,
+    TGrid, Unit, WRect,
 };
 
 struct ActiveDescription {
@@ -69,22 +69,24 @@ impl GridDescription for ActiveDescription {
 }
 
 fn render_active(doc: &PdfDocumentReference, page_bounds: &WRect) -> weekly::Result<Instructions> {
-    let half_page = page_bounds.resize(page_bounds.width() / 2, page_bounds.height());
-    let left_bounds = half_page.inset_all_q1(
-        0.25.inches() + 0.125.inches(),
-        0.25.inches(),
-        0.25.inches(),
-        0.25.inches(),
-    );
-    let right_bounds = left_bounds.move_to(half_page.right() + 0.25.inches(), left_bounds.top());
+    let half_page = page_bounds
+        .resize(page_bounds.width() / 2, page_bounds.height())
+        // A rounding error prevents rendering the last line,
+        // so we add a smidge of extra vertical space.
+        .inset_all_q1(Unit::zero(), Unit::zero(), Unit::zero(), -0.1.inches());
+    let left_bounds =
+        half_page.inset_all_q1(0.25.inches(), 0.25.inches(), 0.125.inches(), 0.25.inches());
+    let right_bounds = left_bounds.move_to(half_page.right() + 0.125.inches(), left_bounds.top());
 
     let mut instructions = Instructions::default();
 
     let task_height = 0.25.inches();
-    draw_tasks_in_bounds(left_bounds, &mut instructions, task_height);
-    //draw_tasks_in_bounds(right_bounds, &mut instructions, task_height);
-
     let font = doc.add_builtin_font(BuiltinFont::HelveticaBold)?;
+
+    let description = ActiveDescription::with_bounds(left_bounds, font.clone(), task_height);
+    let grid = TGrid::with_description(description);
+    grid.append_to_instructions(&mut instructions);
+
     let description = ActiveDescription::with_bounds(right_bounds, font, task_height);
     let grid = TGrid::with_description(description);
     grid.append_to_instructions(&mut instructions);
@@ -98,33 +100,4 @@ fn main() -> weekly::Result<()> {
     let page_bounds = weekly::sizes::letter();
 
     save_one_page_document(doc_title, output_filename, &page_bounds, render_active)
-}
-
-fn draw_tasks_in_bounds(bounds: WRect, instructions: &mut Instructions, task_height: Unit) {
-    instructions.set_stroke_width(0.0);
-
-    let base_check_rect = WRect::with_dimensions(task_height / 2, task_height / 2);
-
-    let mut curr_y = bounds.top() - task_height;
-    while curr_y > bounds.bottom_q1() {
-        instructions.set_stroke_color(&Colors::black());
-        instructions.set_dash(3, 2);
-
-        let line = WLine::line(bounds.left(), curr_y, bounds.right(), curr_y);
-        instructions.push_shape(line.as_pdf_line());
-
-        instructions.set_stroke_color(&Colors::gray(0.5));
-        instructions.clear_dash();
-
-        let check_rect =
-            base_check_rect.move_to(bounds.left() + task_height / 4, curr_y - task_height / 4);
-        if check_rect.bottom_q1() > bounds.bottom_q1() {
-            let mut shape = check_rect.as_pdf_line();
-            shape.has_fill = false;
-            shape.has_stroke = true;
-            instructions.push_shape(shape);
-        }
-
-        curr_y = curr_y - task_height;
-    }
 }

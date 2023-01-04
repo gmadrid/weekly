8aa79f72661631e067bee79f62a21b3747958ea5
George Madrid <gmadrid@gmail.com>
2023-01-04 18:04:31 -0500

    Changing internal document name.

--- src/bin/weekly.rs
+++ src/bin/weekly.rs
@@ -283,6 +283,10 @@ pub fn main() -> Result<()> {
 
     let page_rect = sizes::halfletter();
 
+    let page_rect = sizes::letter();
+    let top_half = page_rect.resize(page_rect.width(), page_rect.height() / 2.0);
+    
+
     save_one_page_document(
         "Productivity Tracker",
         &args.output_filename,
